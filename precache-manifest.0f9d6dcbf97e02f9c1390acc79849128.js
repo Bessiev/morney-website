1568aa21987e27aba45f2558400e96dd5e5633af
Bessie <1976177625@qq.com>
2021-07-14 11:22:55 +0800

    deploy

---
+++ precache-manifest.0f9d6dcbf97e02f9c1390acc79849128.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ded03438ce615f17d3e",
+    "url": "/morney-website/css/app.d755797c.css"
+  },
+  {
+    "revision": "c5511d15dd8f57d007246e6b52b4ac26",
+    "url": "/morney-website/index.html"
+  },
+  {
+    "revision": "0ded03438ce615f17d3e",
+    "url": "/morney-website/js/app.5ded6e56.js"
+  },
+  {
+    "revision": "ecdf0a6627a8efa02cc0",
+    "url": "/morney-website/js/chunk-vendors.eaaf0af3.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/morney-website/robots.txt"
+  }
+]);
